1f84e9f3f0759645a13267c9818e677cff4a61a0
w-gin-tan <wtg28@outlook.com.au>
2023-02-03 11:16:29 +0800

    Write for loop to create & style grid squares.

--- sketch.js
+++ sketch.js
@@ -6,6 +6,16 @@ body.setAttribute('style', 'min-height: 100%; margin: 0; padding: 0; display: fl
 
 const sketchpad = document.createElement('div');
 sketchpad.classList.add('sketchpad');
-sketchpad.setAttribute('style', 'width: 256px; height: 256px; border: 5px solid black;');
+sketchpad.setAttribute('style', 'width: 256px; height: 256px; border: 5px solid black; display: flex; flex-wrap: wrap; flex-direction: column;');
+
+const gridSize = 256;
+const squareSize = Math.sqrt(gridSize);
+
+for (let squares = 0; squares < gridSize; squares++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    square.setAttribute('style', `width: ${squareSize}px; height: ${squareSize}px; border: 0.05px dotted black; box-sizing: border-box;`);
+    sketchpad.appendChild(square);
+}
 
 body.appendChild(sketchpad);
